99c499ebcf507814454444e7d73c4d2c1001d241
Avinash Maurya <avinashm14012001@gmail.com>
2022-04-21 10:36:57 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("This is sum of a + b " + sum); 
+    }
+}
